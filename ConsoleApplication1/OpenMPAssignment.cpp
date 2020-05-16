32f06527b9a2fc4aa4e5c6ba4becde7c36853fc8
hiteshbhavsar <hitesh11oct@gmail.com>
2020-05-15 23:18:23 -0700

    OpenMp Code executed Successfully

---
+++ ConsoleApplication1/OpenMPAssignment.cpp
@@ -0,0 +1,66 @@
+	// OpenMP Code for matrix multiplication
+	// to compute the multiplication of two arrays in parallel
+	#include "stdafx.h"
+	#include<iostream>
+	#include<stdio.h>
+	#include < omp.h >
+	#include<conio.h>
+	#define cols1 100
+	#define rows1 100
+	#define cols2 100
+	#define rows2 100
+	int main()
+	{
+
+	int i, j, k;
+	if (cols1 != rows2)
+	{
+		printf("The dimension of the array defined are incorrect. Please check!");
+		return -1;
+	}
+	int a[rows1][cols1] = { 0 };
+	for (i = 0; i < rows1; i++)
+	{
+		for (j = 0; j < cols1; j++)
+		{
+			a[i][j] = rand();
+		}
+	}
+	int b[rows2][cols2] = { 0 };
+	for (i = 0; i < rows2; i++)
+	{
+		for (j = 0; j < cols2; j++)
+		{
+			b[i][j] = rand();
+		}
+	}
+
+	int result[rows1][cols2] = { 0 };
+
+
+	double time;
+	omp_set_num_threads(4);
+	time = -omp_get_wtime();
+
+	#pragma omp parallel shared(a,b,result) private(i,j,k)
+	{
+		#pragma omp for schedule (static)
+		for (i = 0; i < rows1; i++)
+		{
+			for (j = 0; j < cols2; j++)
+			{
+				result[i][j] = 0;
+				for (k = 0; k < cols1; k++)
+				{
+				result[i][j] += a[i][k] * b[k][j];
+				}
+			}
+
+		}
+	}
+	time = omp_get_wtime() + time;
+	printf("Time Required for the function execution is %lf micro seconds.\n", time* 1000000);
+	getch();
+	return 0;
+	}
+
